da054c04649a1a351da01073c86349a41d21118f
Nahuel-dev <nahualfano@gmail.com>
2021-01-05 20:55:46 -0300

    logica de salto y desplazamiento + perder

--- JuegoSimple/script.js
+++ JuegoSimple/script.js
@@ -1,6 +1,9 @@
 const personaje = document.getElementById('character');
 const block = document.getElementById('block');
 
+
+
+
 function jump(){
     if(personaje.classList != 'jumpAnimation'){
         personaje.classList.add('jumpAnimation')
@@ -9,3 +12,12 @@ function jump(){
         personaje.classList.remove('jumpAnimation')
     }, 700)
 }
+
+const youLose = setInterval(function(){
+    let personajePosition = parseInt(window.getComputedStyle(personaje).getPropertyValue('top'));
+    let blockPosition = parseInt(window.getComputedStyle(block).getPropertyValue('left'));
+    if(blockPosition < 35 && blockPosition > 15 && personajePosition >= 130){
+        block.style.animation = 'none';
+        alert('You lose!!')
+    }
+}, 10);
